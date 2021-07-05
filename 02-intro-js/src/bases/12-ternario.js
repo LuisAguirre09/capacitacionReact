580148ffb68e94988dfb66d61ef818b006c0f1f9
eduardo-af <laguirre@quaxar.com>
2021-07-05 17:59:34 -0500

    section 4 started

--- 02-intro-js/src/bases/12-ternario.js
+++ 02-intro-js/src/bases/12-ternario.js
@@ -1,9 +1,9 @@
 
 const activo = true;
 
-const mensaje = (activo) ? 'Activo' : 'Inactivo'; //operador ternario
+const mensaje1 = (activo) ? 'Activo' : 'Inactivo'; //operador ternario
 
 //const mensaje = ( !activo ) && 'Negativo'; //operador ternario abreviado (retorna false)
-const mensaje = ( activo ) && 'Activo'; //retorna "activo"
+const mensaje2 = ( activo ) && 'Activo'; //retorna "activo"
 
 console.log(mensaje);
--- 02-intro-js/src/index.js
+++
@@ -1,9 +0,0 @@
-
-const activo = true;
-
-const mensaje = (activo) ? 'Activo' : 'Inactivo'; //operador ternario
-
-//const mensaje = ( !activo ) && 'Negativo'; //operador ternario abreviado (retorna false)
-const mensaje = ( activo ) && 'Activo'; //retorna "activo"
-
-console.log(mensaje);

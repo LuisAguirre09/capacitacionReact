11005f9da83a18971179a1d27c01241e0409f842
eduardo-af <laguirre@quaxar.com>
2021-07-06 13:44:57 -0500

    section 4 end

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -10,11 +10,17 @@ const CounterApp = ( {value} ) => {
         setCounter( counter + 1 );
     };
 
+    const handleSubstract = () => { setCounter( counter -1 ) };
+
+    const handleReset = () => { setCounter( value ) };
+
     return (
         <>
             <h1> CounterApp </h1>
             <p> { counter } </p>
             <button onClick= { handleAdd } > +1 </button>
+            <button onClick= { handleReset }> Reset </button>
+            <button onClick= { handleSubstract }> -1 </button>
         </>
     )
 
